93bea015630750fea4b6db03bb1cb577c1a3241e
amzaandrei <andrei.amza13@yahoo.com>
2020-02-10 22:29:49 +0100

    Initial Commit

---
+++ my-swift-ui/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  my-swift-ui
+//
+//  Created by Andrew on 2/10/20.
+//  Copyright © 2020 Andrew. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            VStack{
+                Spacer()
+            }
+            .frame(width: 340, height: 220)
+            .background(.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding()
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
