f39bea2e87064f89b01849cd2195c9dce1dc6a60
amzaandrei <andrei.amza13@yahoo.com>
2020-02-10 23:10:52 +0100

    Complete Design

--- my-swift-ui/ContentView.swift
+++ my-swift-ui/ContentView.swift
@@ -11,37 +11,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            VStack{
-                Spacer()
-            }
-            .frame(width: 340, height: 220)
-            .background(.blue)
-            .cornerRadius(20)
-            .shadow(radius: 20)
-            .offset(x: 0, y: -20)
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("UI Design")
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                        Text("Certificate")
-                            .foregroundColor(Color("accent"))
-                    }
-                    Spacer()
-                    Image("Logo1")
-                }
-                .padding()
-                Image("Card1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 300, height: 110, alignment: .top)
-            }
-            .frame(width: 340.0, height: 220.0)
-            .background(Color.black)
-            .cornerRadius(20)
-            .shadow(radius: 20)
+            TitleView()
+                .blur(radius: 20)
+            
+            BackCardView()
+                .frame(width: 340, height: 220)
+                .background(Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(10))
+                .rotation3DEffect(.degrees(10), axis: (x: 10, y: 0, z: 0))
+                .blendMode(.hardLight)
+            
+            BackCardView()
+                .frame(width: 340, height: 220)
+                .background(Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: -20)
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(.degrees(5), axis: (x: 10, y: 0, z: 0))
+                .blendMode(.hardLight)
+            CardView()
+                .blendMode(.hardLight)
+                .blur(radius: 20)
+            
         }
     }
 }
@@ -49,5 +46,83 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct CardView: View {
+    var body: some View{
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+
+struct BackCardView: View {
+    var body: some View {
+        VStack{
+            Spacer()
+        }
+        
+    }
+}
+
+
+struct TitleView: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Certificate")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20){
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("Up maids me an ample stood given. Certainty say suffering his him collected intention promotion. Hill sold ham men made lose case. Views abode law heard jokes too.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding()
+        .background(Color.white)
+        .frame(maxWidth: .infinity)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 500)
     }
 }
